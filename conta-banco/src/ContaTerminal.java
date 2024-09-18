ba47b92d73cb2ac4809357516c7eb7d5de4e6444
eidelweip <pollyana.eidelwein@gmail.com>
2024-09-18 11:24:41 -0300

    Projeto com as funcionalidades definidas pelo desafio.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+/**
+ * <h1>ContaTerminal</h1>
+ * <p>A Conta Terminal permite criar uma nova conta em nosso banco.</p>
+ * <b>Note:</b> Leia atentamente a documentação dessa classe para desfrutar dos recursos oferecidos pelo autor.
+ * 
+ * @author Pollyana Eidelwein
+ * @version 1.0
+ * @since 18/09/2024 
+ */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o seu nome:");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite o código da sua agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite o número da sua conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o quanto tem no seu saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá, "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "
+        +agencia+", conta "+numeroConta+"e seu saldo "+saldo+" já está disponível para saque.");
     }
 }
